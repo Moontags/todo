8f1a21e0816bdd1d23aa3400d400478f89a2e980
Jari Peltola <jena9988@gmail.com>
2025-03-15 19:14:50 +0200

    Next.config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // Varmistaa, että Next.js käyttää `src/app/` -rakennetta
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
